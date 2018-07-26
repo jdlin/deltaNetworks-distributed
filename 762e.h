a3a16c1c1a5ed951a1ef1c5af1da8bd21eae08e8
J. D. Lin <jdlin@users.noreply.github.com>
2018-07-26 17:59:55 +0800

    Create 762e.h

---
+++ 762e.h
@@ -0,0 +1,12 @@
+/*------------------------------------------------------------------------*/
+/* Delta Network Parameters */
+/*------------------------------------------------------------------------*/
+#define a      4
+#define b      4
+#define n      4
+#define N    256
+#define P      4
+#define TIMES  100
+
+#define DATA   1
+#define EMPTY -1
